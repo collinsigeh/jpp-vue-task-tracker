e505e4f7bc5da5e5e03d0e7e2df8e043f80c3e10
Collins Igeh <igehac@gmail.com>
2022-08-04 14:57:10 +0100

    added conditional class

---
+++ src/components/Task.vue
@@ -0,0 +1,35 @@
+<script>
+    export default {
+        name: 'Task',
+        props: {
+            task: Object
+        }
+    }
+</script>
+
+<template>
+    <div :class="[task.reminder ? 'reminder' : '', 'task']">
+        <h1>{{ task.text }} <i class="fas fa-times-circle"></i></h1>
+        <p>{{ task.day }}</p>
+    </div>
+</template>
+
+<style scoped>
+.fas {
+  color: red;
+}
+.task {
+  background: #f4f4f4;
+  margin: 5px;
+  padding: 10px 20px;
+  cursor: pointer;
+}
+.task.reminder {
+  border-left: 5px solid green;
+}
+.task h3 {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+</style>
--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -1,14 +1,18 @@
 <script>
-    export default {
-        name: 'Tasks',
-        props: {
-            tasks: Array
-        }
-    }
+import Task from './Task.vue';
+
+export default {
+    name: "Tasks",
+    props: {
+        tasks: Array
+    },
+    components: { Task }
+}
 </script>
 
 <template>
     <div v-for="task in tasks" :key="task.id">
-        <h3>{{ task.text }}</h3>
+        <Task :task="task" />
     </div>
 </template>
+
