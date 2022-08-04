30ff6ac4ea148b3785ad06d7c739cb51fe8e3ff3
Collins Igeh <igehac@gmail.com>
2022-08-04 13:30:27 +0100

    created the task component

---
+++ src/components/Tasks.vue
@@ -0,0 +1,14 @@
+<script>
+    export default {
+        name: 'Tasks',
+        props: {
+            tasks: Array
+        }
+    }
+</script>
+
+<template>
+    <div v-for="task in tasks" :key="task.id">
+        <h3>{{ task.text }}</h3>
+    </div>
+</template>
